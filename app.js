421132c471d47885cbcbe8f46190c839f0414a07
yashwanth171 <yashwanthbv17@gmail.com>
2022-05-23 00:11:53 +0530

    adding files

--- app.js
+++ app.js
@@ -2,24 +2,22 @@ const request = require('request');
 const geocode = require('./geocode');
 const forecast = require('./forecast');
 
-const address = ''
-if (!address){
-    console.log('error: Please provide address')
+const fetchGeoCode = (address,callback) => {
+    geocode(address,(error,{latitude, longitue, location} = {}) => {
+        if (error){
+            console.log('error:',error)
+            callback(error)
+        }
+        else {
+            forecast(latitude, longitue,(error, forecastData) =>{
+                if (error){
+                    console.log('error:',error)
+                }
+                else{
+                    console.log(forecastData)
+                }
+            })
+        }
+    })
 }
-else{
-geocode(address,(error,{latitude, longitue, location} = {}) => {
-    if (error){
-        console.log('error:',error)
-    }
-    else {
-        forecast(latitude, longitue,(error, forecastData) =>{
-            if (error){
-                console.log('error:',error)
-            }
-            else{
-                console.log(forecastData)
-            }
-        })
-    }
-})
-}
+module.exports = fetchGeoCode
